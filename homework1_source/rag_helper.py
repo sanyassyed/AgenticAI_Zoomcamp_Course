553fe28a53ae998a72a0551a2bf512b1edd23c4c
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2026-06-24 02:09:51 +0000

    ADD: Homework 1 completed

--- homework1_source/rag_helper.py
+++ homework1_source/rag_helper.py
@@ -23,34 +23,27 @@ class RAGBase:
         llm_client,
         instructions=INSTRUCTIONS,
         prompt_template=PROMPT_TEMPLATE,
-        course='llm-zoomcamp',
         model='gpt-5.4-mini'
     ):
         self.index = index
         self.llm_client = llm_client
         self.instructions = instructions
-        self.course = course
         self.prompt_template = prompt_template
         self.model = model
 
     def search(self, query, num_results=5):
-        boost_dict = {'question': 3.0, 'section': 0.5}
-        filter_dict = {'course': self.course}
 
         return self.index.search(
             query,
             num_results=num_results,
-            boost_dict=boost_dict,
-            filter_dict=filter_dict
         )
 
     def build_context(self, search_results):
         lines = []
 
         for doc in search_results:
-            lines.append(doc['section'])
-            lines.append('Q: ' + doc['question'])
-            lines.append('A: ' + doc['answer'])
+            lines.append('FILE NAME: ' + doc['filename'])
+            lines.append('CONTENT: ' + doc['content'])
             lines.append('')
 
         return '\n'.join(lines).strip()
@@ -71,6 +64,8 @@ class RAGBase:
             model=self.model,
             input=input_messages
         )
+
+        print(f"Response Details: {response.usage}")
 
         return response.output_text
 
